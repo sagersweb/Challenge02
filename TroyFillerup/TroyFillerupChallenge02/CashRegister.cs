3b4fc05311ea529fb2d0a9c31a959a6f0ee7bfc8
tfillerup <troy.fillerup@gmail.com>
2014-11-25 08:32:33 -0700

    Troy Fillerup

---
+++ TroyFillerup/TroyFillerupChallenge02/CashRegister.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TroyFillerupChallenge02
+{
+    public class CashRegister
+    {
+        public Dictionary<double, int> GetChange(double amount, List<double> denominationArray)
+        {
+            if (amount >= 0)
+            {
+                var retval = new Dictionary<double, int>();
+
+                double amountRemaining = amount;
+
+                denominationArray.ForEach(denom =>
+                {
+                    int moneyCount = 0;
+                    amountRemaining = CalculateDenomination(denom, amountRemaining, out moneyCount);
+                    retval.Add(denom, moneyCount);
+                });
+
+                return retval; 
+            }
+            else
+            {
+                throw new Exception("Amount must be greater than zero.");
+            }
+        }
+
+        private static double CalculateDenomination(double denomination, double amount, out int moneyCount)
+        {
+            moneyCount = (int)(amount / denomination);
+            var newAmount = (amount - (moneyCount * denomination));
+            return newAmount;
+        }
+    }
+}
